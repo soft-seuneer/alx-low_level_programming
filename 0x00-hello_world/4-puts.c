14271fbc7af1fe30063d28c45dec496aa43bedc3
oyeniran oluwaseun <oluwaseunoye100@gmail.com>
2022-09-08 19:46:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  *
  * main - print the string in the put function
  *
- * Description: using the main function
- * this program prints "Programming is lke building a multilingual puzzle 
+ * Description: using the main function this program prints "Programming is lke building a multilingual puzzle 
  * Return: 0
  */
 int main(void)
