1a619475268a320e0f548ad83e775e81a60db520
oyeniran oluwaseun <oluwaseunoye100@gmail.com>
2022-09-08 19:38:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is lke building a multilingual puzzle 
+ * Return: 0
+ */
+int main(void)
+{	puts("\"Programmming is like building a multilingual puzzle");
+	return (0);
+}
