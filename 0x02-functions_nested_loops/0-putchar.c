bd34cc7cf7b2e5db19c6c04f9c41f4680f1d633c
oyeniran oluwaseun <oluwaseunoye100@gmail.com>
2022-09-14 16:46:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,14 @@
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('r');
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int coint, s;
+
+	s = sizeof(str) / sizeof(int);
+	for (count = 0; count < s; count++)
+	{
+		_putchar(str[count]);
+	}
 	_putchar('\n');
 
 	return (0);
