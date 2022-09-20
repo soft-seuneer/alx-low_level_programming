f9f4eea2187d02b6c922850bc870a560e28c3300
oyeniran oluwaseun <oluwaseunoye100@gmail.com>
2022-09-19 17:38:52 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,15 +2,16 @@
 /**
  * _puts - prints a string
  * @str: string to print
- *
  * Description: prints a string
- * On success: return the number of characters printed
+ * Return: no return
  */
 
 void _puts(char *str)
 {
-	while (*str)
-	_putchar(*str++);
-
+	while (*str != '\0')
+	{
+	_putchar(*str);
+	str++;
+	}
 	_putchar('\n');
 }
