2c9f5d133441820795ed85266a09ba5c0970f04f
oyeniran oluwaseun <oluwaseunoye100@gmail.com>
2022-09-21 12:17:35 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,23 +2,17 @@
 /**
  * reverse_array - reverse
  * @a : pointer to int params
- * @n : pointer to int params
- * Return: no
+ * @n : array length
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int j;
-	int tmp;
+	int i = 0, half;
 
-	i = 0;
-	j = n - 1;
-
-	while (i < j)
+	for (half = n / 2; half > 0; half--, i++)
 	{
-		tmp = a[i];
-		a[i] = a[j];
-		i++;
-		j--;
+		a[n - i - 1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n - i - 1] = a[n - i - 1] - a[i];
 	}
 }
